ba80388da6c12a4339baaf5b9e6b1b09b7b0d150
J.E. R <5433476+je-r@users.noreply.github.com>
2022-09-19 15:08:00 +0200

    Update timezones.sql

--- timezones.sql
+++ timezones.sql
@@ -1,7 +1,7 @@
 select  SYSDATETIMEOFFSET() AT TIME ZONE 'Central European Standard Time' as 'col1', 
         GETDATE() as 'col2'
         
-/* sample result:
+/* sample result when time on watches was 15:06:
 
 col1	col2
 2022-09-19 15:06:32.5748218 +02:00	2022-09-19 13:06:32.570
